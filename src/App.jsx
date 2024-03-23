afe2c70b5885230b4fcf71f4f6ba35da4e690889
Leonardo Mendes <mendesleonardo475@gmail.com>
2024-03-23 10:59:44 -0300

    fix: fix import name

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 import Navbar from './components/nav'
-import Home from './pages/Home'
+import Home from './pages/home'
 import ComicsPage from './pages/Comics'
 import Footer from './components/footer'
 
